e8481e0e64f913679de0bf3de48e57a4cc63ad65
Ilia Zaremba <zarembais@gmail.com>
2020-05-03 12:47:51 +0300

    Release 0

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css'
+import Profile from './components/Profile/Profile';
 
 export class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello React</h1>
+        <Profile />
       </div>
     )
   }
---
+++ frontend/src/components/Profile/Profile.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+
+
+class Profile extends Component {
+
+
+render() {
+  return (<Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" width='360px' src="https://342031.selcdn.ru/rusplt/1733/2223/Pel_ttserGL.png" rounded />
+  <Card.Body>
+    <Card.Title>Татьяна Пельтцер</Card.Title>
+    <Card.Subtitle className="mb-2 text-muted">73 года</Card.Subtitle>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="secondary" size="lg" active>Go somewhere</Button>
+    
+  </Card.Body>
+</Card>)
+
+}
+}
+
+export default Profile
