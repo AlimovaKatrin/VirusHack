83bf0b93585a0fecea3e8b271efaef264f65e7c3
Katrin <alimova.katrin8@gmail.com>
2020-05-04 12:13:12 +0300

    Connect everything in one

--- frontend/src/components/Profile/Profile.js
+++ frontend/src/components/Profile/Profile.js
@@ -12,27 +12,6 @@ class Profile extends Component {
 
   render() {
     return (
-<<<<<<< Updated upstream
-
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" width='360px' src="https://342031.selcdn.ru/rusplt/1733/2223/Pel_ttserGL.png" rounded />
-        <Card.Body>
-          <Card.Title style={{ color: "#047B7C" }}>Татьяна Пельтцер</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">73 года</Card.Subtitle>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
-          </Card.Text>
-          <ButtonGroup aria-label="Basic example">
-            <Button variant="secondary" style={{ backgroundColor: "#047B7C" }} size="mg" active>Дневник боли</Button>
-            <Button variant="secondary" style={{ backgroundColor: "#047B7C" }} size="mg" active>План ухода</Button>
-          </ButtonGroup>
-        </Card.Body>
-      </Card>
-
-
-
-=======
       <Container fluid>
         <br></br>
         <Row>
@@ -44,7 +23,6 @@ class Profile extends Component {
      
       
       </Container>
->>>>>>> Stashed changes
     )
 
   }
